6e6dfcc44c18767146c4971647507eb9beb02e88
raftum <symbianos15@wp.pl>
2019-03-19 17:52:53 +0100

    rozpoczecie nowy projekt

---
+++ main.c
@@ -0,0 +1,18 @@
+/*
+ * praca_inzynierska.c
+ *
+ * Created: 2019-03-19 17:44:41
+ * Author : Rafal
+ */ 
+
+#include <avr/io.h>
+
+//nowy projekt
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
